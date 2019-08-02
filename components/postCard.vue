115d89644eab7da936251d122bc223c18c388be9
cjustinobi <cjustinobi@gmail.com>
2019-08-02 14:29:32 +0100

    add blog

---
+++ components/postCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <article class="article-card" >
+        <nuxt-link :to="`/blog/${articleInfo.link}`">
+            <div class="card-inner">
+                <p class="type">
+                    {{ capitalize(articleInfo.attributes.type) }}
+                </p>
+                <h4>{{ articleInfo.attributes.title }}</h4>
+            </div>
+        </nuxt-link>
+    </article>
+</template>
+
+<script>
+    export default {
+        props: {
+            articleInfo: {
+                type: Object,
+                default: () => {}
+            },
+            index: {
+                type: Number,
+                default: 0
+            }
+        },
+        data() {
+            return {
+                size: 'two-thirds',
+                language: this.articleInfo.attributes.language,
+                icon: 'js'
+            }
+        },
+
+        methods: {
+            capitalize(s) {
+                if (typeof s !== 'string') return ''
+                return s.charAt(0).toUpperCase() + s.slice(1)
+            }
+        }
+    }
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>blogs</h1>
+        <section>
+            <PostCard v-for="(blog, index) in blogList" :key="index" :articleinfo="blog"/>
+        </section>
+    </div>
+</template>
+
+<script>
+
+    import PostCard from '../../components/postCard'
+    import blogs from '../../contents/blogs'
+
+    export default {
+        components: { PostCard },
+        async asyncData({ app }) {
+            async function awaitImport(blog) {
+                const wholeMD = await import(`~/contents/blog/${blog.slug}.md`)
+                return {
+                    attributes: wholeMD.attributes,
+                    link: blog.slug
+                }
+            }
+
+            return await Promise.all(blogs.map(blog => awaitImport(blog))
+            ).then(res => {
+                return {
+                    blogList: res
+                }
+            })
+        }
+    }
+</script>
