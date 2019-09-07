936ffe19abd4186fa0f7bc27957a0671ba58cfc5
cjustinobi <cjustinobi@gmail.com>
2019-09-07 16:18:16 +0100

    add porfolio interface

--- components/MobileFooter.vue
+++ components/MobileFooter.vue
@@ -1,82 +1,82 @@
 <template>
-    <div class="mobile-footer">
-        <span class="about"><nuxt-link :to="homeAbout"><i :class="homeAboutIcon"></i></nuxt-link></span>
-        <span class="blog"><nuxt-link to="/blog"><i class="fa fa-book"></i></nuxt-link></span>
-        <span class="portfolio"><nuxt-link to="/portfolio"><i class="fa fa-code"></i></nuxt-link></span>
-        <span class="contactt"><nuxt-link to="/contact"><i class="fa fa-address-card"></i></nuxt-link></span>
-    </div>
+  <div class="mobile-footer">
+    <span class="about"><nuxt-link :to="homeAbout"><i :class="homeAboutIcon"></i></nuxt-link></span>
+    <span class="blog"><nuxt-link to="/blog"><i class="fa fa-book"></i></nuxt-link></span>
+    <span class="portfolio"><nuxt-link to="/portfolio"><i class="fa fa-code"></i></nuxt-link></span>
+    <span class="contactt"><nuxt-link to="/contact"><i class="fa fa-address-card"></i></nuxt-link></span>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                homeAbout: '/about',
-                homeAboutIcon: 'fa fa-user-circle'
-            }
-        },
+  export default {
+    data() {
+      return {
+        homeAbout: '/about',
+        homeAboutIcon: 'fa fa-user-circle'
+      }
+    },
 
-        watch: {
-            '$route': function(e) {
-                if(e.path == '/') {
-                    this.homeAbout = '/about'
-                    this.homeAboutIcon = 'fa fa-user-circle'
-                } else {
-                    this.homeAbout = '/'
-                    this.homeAboutIcon = 'fa fa-home'
-                }
-            }
+    watch: {
+      '$route': function(e) {
+        if(e.path == '/') {
+          this.homeAbout = '/about'
+          this.homeAboutIcon = 'fa fa-user-circle'
+        } else {
+          this.homeAbout = '/'
+          this.homeAboutIcon = 'fa fa-home'
         }
+      }
     }
+  }
 </script>
 
 <style scoped>
-    a{
-        outline: none;
-    }
-    .mobile-footer{
-        display: -ms-grid;
-        display: grid;
-        -ms-grid-rows: 45px;
-        -ms-grid-columns: (1fr)[3];
-        grid-template: 45px / repeat(4, 1fr);
-        text-align: center;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        z-index: 1000;
-    }
-    .about{
-        display: -ms-grid;
-        display: grid;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        align-items: center;
-        background: #FF4081;
-    }
-    .blog{
-        display: -ms-grid;
-        display: grid;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        align-items: center;
-        background: #465bff;
-    }
-    .portfolio{
-        display: -ms-grid;
-        display: grid;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        align-items: center;
-        background: #94cd14;
-    }
-    .contactt{
-        display: -ms-grid;
-        display: grid;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        align-items: center;
-        background: #ffc100;
-    }
+  a{
+    outline: none;
+  }
+  .mobile-footer{
+    display: -ms-grid;
+    display: grid;
+    -ms-grid-rows: 45px;
+    -ms-grid-columns: (1fr)[3];
+    grid-template: 45px / repeat(4, 1fr);
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
+  }
+  .about{
+    display: -ms-grid;
+    display: grid;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    background: #FF4081;
+  }
+  .blog{
+    display: -ms-grid;
+    display: grid;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    background: #465bff;
+  }
+  .portfolio{
+    display: -ms-grid;
+    display: grid;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    background: #94cd14;
+  }
+  .contactt{
+    display: -ms-grid;
+    display: grid;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    background: #ffc100;
+  }
 </style>
