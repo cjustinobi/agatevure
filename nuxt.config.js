13759b23243f1cd48313d4ee7baedd24f7b963a7
cjustinobi <cjustinobi@gmail.com>
2019-09-10 06:44:33 +0100

    add Google Analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,53 +2,60 @@ import blogs from './contents/blogs.json'
 
 
 export default {
-    mode: 'universal',
+  mode: 'universal',
 
-    generate: {
-        routes: [].concat(blogs.map(blog => `/blog/${blog.slug}`))
-    },
+  generate: {
+    routes: [].concat(blogs.map(blog => `/blog/${blog.slug}`))
+  },
 
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
-    loading: { color: '#FF4081' },
-
-    css: [
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
 
-    plugins: [
+  loading: { color: '#FF4081' },
+
+  css: [
+  ],
+
+  plugins: [
+  ],
+
+  devModules: [
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    ['@nuxtjs/google-tag-manager', {
+      id: 'GTM-MTKH8ZH',
+      pageTracking: true,
+      scriptURL: '//agatevure.com',
+      noscriptURL: '//agatevure.com'
+      }
     ],
+  ],
 
-    devModules: [
-    ],
+  axios: {
+  },
 
-    modules: [
-        '@nuxtjs/axios',
-        '@nuxtjs/pwa',
-    ],
-
-    axios: {
-    },
-
-    build: {
-        extractCSS: true,
-        extend(config, ctx) {
-            config.module.rules.push({
-                test: /\.md$/,
-                loader: 'frontmatter-markdown-loader',
-                options: {
-                    vue: true
-                }
-            })
+  build: {
+    extractCSS: true,
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader',
+        options: {
+          vue: true
         }
+      })
     }
+  }
 }
